ea07a15e1cad7083e21e2d95a9755452f2221c43
tan-yuki <a0673040@gmail.com>
2017-11-18 17:24:08 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var timeout = require('connect-timeout');
+var app = express();
+
+app.get('/', function (req, res) {
+  sleep(5000).then(function() {
+    res.send('Hello World!');
+  });
+});
+
+app.get('/customize_timeout', timeout('2s'), function (req, res) {
+  sleep(5000).then(function() {
+    res.send('Hello World!');
+  });
+});
+
+app.listen(3333, function () {
+  console.log('Example app listening on port 3333!');
+});
+
+function sleep(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
